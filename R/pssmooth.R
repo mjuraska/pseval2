6ee081d741b98c7ee72681fb9b162257c3969348
Michal Juraska <mjuraska@fhcrc.org>
2018-07-23 11:16:19 -0700

    Revised the example for summary.riskCurve() to run in <10 s to pass CRAN's check

--- R/pssmooth.R
+++ R/pssmooth.R
@@ -802,7 +802,6 @@ invtContrastRiskCurve <- function(x, contrast){
 #' S <- MASS::mvrnorm(n, mu=c(2,2,3), Sigma=matrix(c(1,0.9,0.7,0.9,1,0.7,0.7,0.7,1), nrow=3))
 #' p <- pnorm(drop(cbind(1,Z,(1-Z)*S[,2],Z*S[,3]) %*% c(-1.2,0.2,-0.02,-0.2)))
 #' Y <- sapply(p, function(risk){ rbinom(1,1,risk) })
-#' X <- rbinom(n,1,0.5)
 #' # delete S(1) in placebo recipients
 #' S[Z==0,3] <- NA
 #' # delete S(0) in treatment recipients
@@ -813,13 +812,13 @@ invtContrastRiskCurve <- function(x, contrast){
 #' S[Y==0 & phase2==0,] <- c(NA,NA,NA)
 #' # delete Sb in cases not included in the phase 2 subset
 #' S[Y==1 & phase2==0,1] <- NA
-#' data <- data.frame(X,Z,S[,1],ifelse(Z==0,S[,2],S[,3]),Y)
-#' colnames(data) <- c("X","Z","Sb","S","Y")
+#' data <- data.frame(Z,S[,1],ifelse(Z==0,S[,2],S[,3]),Y)
+#' colnames(data) <- c("Z","Sb","S","Y")
 #' qS <- quantile(data$S, probs=c(0.05,0.95), na.rm=TRUE)
 #' grid <- seq(qS[1], qS[2], length.out=2)
 #'
-#' out <- riskCurve(formula=Y ~ S + factor(X), bsm="Sb", tx="Z", data=data, biomarkerGrid=grid)
-#' boot <- bootRiskCurve(formula=Y ~ S + factor(X), bsm="Sb", tx="Z", data=data,
+#' out <- riskCurve(formula=Y ~ S, bsm="Sb", tx="Z", data=data, biomarkerGrid=grid)
+#' boot <- bootRiskCurve(formula=Y ~ S, bsm="Sb", tx="Z", data=data,
 #'                       biomarkerGrid=grid, iter=2, seed=10)
 #' summary(out, boot, contrast="te")
 #'
